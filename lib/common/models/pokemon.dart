26cc55c775fb4fe766c68c07a0640909b222f31e
Francielxyz <ruanvha15@gmail.com>
2022-03-14 21:42:59 -0300

    criando telas

--- lib/common/models/pokemon.dart
+++ lib/common/models/pokemon.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class Pokemon {
   final int id;
   final String num;
@@ -11,9 +13,15 @@ class Pokemon {
       num: json['num'],
       name: json['name'],
       img: json['img'],
-      type: (json['type'] as List<dynamic>).map((e) => e as String,).toList(),
+      type: (json['type'] as List<dynamic>)
+          .map(
+            (e) => e as String,
+          )
+          .toList(),
     );
   }
+
+  Color? get baseColor => _color(type: type[0]);
 
   Pokemon({
     required this.id,
@@ -22,4 +30,47 @@ class Pokemon {
     required this.img,
     required this.type,
   });
+
+  static Color? _color({required String type}) {
+    switch (type) {
+      case 'Normal':
+        return Colors.brown[400];
+      case 'Fire':
+        return Colors.red;
+      case 'Water':
+        return Colors.blue;
+      case 'Grass':
+        return Colors.green;
+      case 'Electric':
+        return Colors.amber;
+      case 'Ice':
+        return Colors.cyanAccent[400];
+      case 'Fighting':
+        return Colors.orange;
+      case 'Poison':
+        return Colors.purple;
+      case 'Ground':
+        return Colors.orange[300];
+      case 'Flying':
+        return Colors.indigo[200];
+      case 'Psychic':
+        return Colors.pink;
+      case 'Bug':
+        return Colors.lightGreen[500];
+      case 'Rock':
+        return Colors.grey;
+      case 'Ghost':
+        return Colors.indigo[400];
+      case 'Dark':
+        return Colors.brown;
+      case 'Dragon':
+        return Colors.indigo[800];
+      case 'Steel':
+        return Colors.blueGrey;
+      case 'Fairy':
+        return Colors.pinkAccent[100];
+      default:
+        return Colors.grey;
+    }
+  }
 }
--- lib/features/pokedex/screens/details/container/detail_container.dart
+++ lib/features/pokedex/screens/details/container/detail_container.dart
@@ -7,9 +7,9 @@ import 'package:pokedex/common/widgets/po_loading.dart';
 import 'package:pokedex/features/pokedex/screens/details/pages/detail_page.dart';
 
 class DetailArguments {
-  DetailArguments({required this.name});
+  DetailArguments({required this.pokemon});
 
-  final String name;
+  final Pokemon pokemon;
 }
 
 class DetailContainer extends StatelessWidget {
@@ -32,7 +32,7 @@ class DetailContainer extends StatelessWidget {
           if (snapshot.connectionState == ConnectionState.done &&
               snapshot.hasData) {
             return DetailPage(
-              name: arguments.name,
+              pokemon: arguments.pokemon,
               list: snapshot.data!,
             );
           }
